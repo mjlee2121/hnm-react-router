e10d9937a0eb7d54a965e3a86bc7b9a9a53647fd
minji <lee.minji9521@gmail.com>
2024-07-03 13:43:53 -0400

    search bar and hnm logo

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Routes, Route} from 'react-router-dom'
 import ProductAll from './page/ProductAll';
 import Login from './page/Login';
 import ProductDetail from './page/ProductDetail';
+import Navbar from './component/Navbar';
 
 function App() {
   return (
     <div className="App">
       <h1> Landing page </h1>
+      <Navbar />
       <Routes>
         <Route path='/product' element={<ProductAll />} />
         <Route path='/login' element={<Login />} />
